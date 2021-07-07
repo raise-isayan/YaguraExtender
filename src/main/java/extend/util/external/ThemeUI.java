266b8186bd4196ad9d6e254b9e24179e9a3ee9d1
isayan <raise.isayan@gmail.com>
2021-07-07 22:08:45 +0900

    fix Theme

--- src/main/java/extend/util/external/ThemeUI.java
+++ src/main/java/extend/util/external/ThemeUI.java
@@ -1,32 +1,40 @@
 package extend.util.external;
 
-import burp.BurpExtender;
-import extension.helpers.StringUtil;
-import java.io.IOException;
-import java.util.Properties;
 import javax.swing.UIManager;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.RSyntaxUtilities;
-import org.fife.ui.rsyntaxtextarea.Theme;
 import org.fife.ui.rtextarea.Gutter;
-import org.fife.ui.rtextarea.RTextScrollPane;
 
 /**
  *
  * @author isayan
  */
 public class ThemeUI {
-      
+
+    /***
+        StringBuilder builder = new StringBuilder();
+        BurpExtender.outPrintln("=====================");
+        Enumeration<Object> key = UIManager.getDefaults().keys();
+        while (key.hasMoreElements()) {
+            builder.append(key.nextElement());
+            builder.append("\n");
+        }
+        BurpExtender.outPrintln(builder.toString());
+        BurpExtender.outPrintln("=====================");
+    **/
+
    public static void changeStyleTheme(RSyntaxTextArea textArea) {
         try {
-            textArea.setForeground(UIManager.getColor("Button.default.foreground"));
-            textArea.setBackground(UIManager.getColor("Button.default.background"));
+            textArea.setForeground(UIManager.getColor("EditorPane.foreground"));
+            textArea.setBackground(UIManager.getColor("TextField.background"));
+            textArea.setSelectedTextColor(UIManager.getColor("TextArea.selectedForeground"));
+            textArea.setSelectionColor(UIManager.getColor("TextArea.selectedBackground"));
             Gutter gutter = RSyntaxUtilities.getGutter(textArea);
             if (gutter!=null) {
-                gutter.setBackground(UIManager.getColor("Button.default.background"));
-                gutter.setLineNumberColor(UIManager.getColor("Button.default.foreground"));
+                gutter.setBackground(UIManager.getColor("EditorPane.foreground"));
+                gutter.setLineNumberColor(UIManager.getColor("TextField.background"));
             }
-        } catch (NullPointerException ex) { 
+        } catch (NullPointerException ex) {
         }
    }
    
